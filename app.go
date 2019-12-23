15d6bb5f30dd3c066de41a7c1ea1091dc4805187
shpark <pica415@gmail.com>
2019-12-23 13:11:36 +0900

    first commit

---
+++ app.go
@@ -0,0 +1,37 @@
+package hellochain
+
+import (
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
+
+	"github.com/cosmos/sdk-tutorials/hellochain/starter"
+)
+
+const appName = "hellochain"
+
+var (
+	// ModuleBasics holds the AppModuleBasic struct of all modules included in the app
+	ModuleBasics = starter.ModuleBasics
+)
+
+type helloChainApp struct {
+	*starter.AppStarter // helloChainApp extends starter.AppStarter
+}
+
+// NewHelloChainApp returns a fully constructed SDK application
+func NewHelloChainApp(logger log.Logger, db dbm.DB) abci.Application {
+
+	// construct our starter to extend
+	appStarter := starter.NewAppStarter(appName, logger, db)
+
+	//compose our app with starter
+	var app = &helloChainApp{
+		appStarter,
+	}
+
+	// do some final configuration...
+	app.InitializeStarter()
+
+	return app
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ykc415/hellochain
+
+go 1.13
+
+require (
+	github.com/cosmos/cosmos-sdk v0.37.4
+	github.com/cosmos/sdk-tutorials/hellochain v0.0.0-20191220075416-88e7370e9da4
+	github.com/tendermint/tendermint v0.32.8
+	github.com/tendermint/tm-db v0.3.0
+)
